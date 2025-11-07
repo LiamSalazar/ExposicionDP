3a741ff1847a1f46c3bc90dcc333b48dcf617e7d
LuisMelendez <luiguimelendez5@gmail.com>
2025-11-07 08:02:56 -0600

    Add recursive function for binomial coefficient

---
+++ BinomialRecursivo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int binomialCoeff(int n, int k) {
+
+    if (k == 0 || k == n)
+        return 1;
+
+    return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k);
+}
+
+
+int main() {
+    int n = 5, k = 2;
+    printf("%d\n", binomialCoeff(n, k));
+    return 0;
+}
+
