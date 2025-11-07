04bbd6b8142d8580a54d03db81a905f38c1dac97
LuisMelendez <luiguimelendez5@gmail.com>
2025-11-07 08:01:08 -0600

    Add binomial coefficient calculation using DP
    Implement a dynamic programming approach to calculate binomial coefficients.

---
+++ BinomialDP.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int binomialCoeff(int n, int k) {
+    int dp[100][100] = {0}; 
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= (i < k ? i : k); j++) { 
+            if (j == 0 || j == i)
+                dp[i][j] = 1;
+            else
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+        }
+    }
+
+    return dp[n][k];
+}
+
+int main() {
+    int n = 5, k = 2;
+    printf("%d\n", binomialCoeff(n, k));
+    return 0;
+}
